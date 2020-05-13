04e426887e8fcd2314a6f37f7015419ec6fd1ea7
yndyk <suivivor0519@gmail.com>
2020-05-13 14:37:57 +0900

    //現段階の変更したウンコをそちらに送ります

--- ポップスズ5月製作/unti.cpp
+++ ポップスズ5月製作/unti.cpp
@@ -1,9 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include "main.h"
 #include"player.h"
 #include"unti.h"
 
 int UntiImage;
 CHARACTER unti;
+int UntiCount;
 
 //ロード
 void SysinitUnti()
@@ -14,21 +18,39 @@ void SysinitUnti()
 //初期化
 void initUnti() 
 {
-	unti.pos = { 400,350 };
+	unti.pos = { 64 + rand() % 18 - 1 * 32,0 };
 	unti.size = { UNTI_SIZE_X,UNTI_SIZE_Y };
-	unti.flag = false;
-	unti.speed = 6;
-
+	unti.flag = false; //表示判定
+	unti.speed = 6; 
+	unti.count = 0;
+	UntiCount = 0;
 }
 
 //更新
 void UpdetaUnti()
 {
-	
-	if (unti.flag == false)
+	srand(time(NULL));
+
+	unti.count++;
+	if (unti.count % 120 == 0)
 	{
-		//unti.pos.y += unti.speed;
+		unti.flag == true;
 	}
+	if(unti.flag == true)
+	{
+		unti.pos.y += unti.speed;
+	}
+	else
+	{
+		unti.flag = true;
+	}
+
+	if (unti.pos.y + 32 < 0)
+	{
+		unti.flag = false;
+		unti.pos = { 64 + (rand() % 18 - 1) * 32,  + 32 };
+	}
+
 		if ((player.pos.x < unti.pos.x + unti.size.x
 			&&unti.pos.x < player.pos.x + player.size.x
 			&&player.pos.y < unti.pos.y + unti.size.y
@@ -39,20 +61,18 @@ void UpdetaUnti()
 			player.hp -= 1;
 		}
 
-		if (unti.flag == true)
+		/*if (unti.flag == true)
 		{
-			
 			unti.pos.x = GetRand(320);
-			
 			unti.flag = false;
-		}
+		}*/
 	
 }
 
 //描画
 void DrawUnti()
 {
-	if (unti.flag == false)
+	if (unti.flag == true)
 	{
 		DrawGraph(unti.pos.x, unti.pos.y, UntiImage, true);
 		/*DrawBox(unti.pos.x, unti.pos.y,
