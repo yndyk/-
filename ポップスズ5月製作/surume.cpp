bbb1caf234782d0dee404df7ad5fe1eef22ea1b1
yndyk <suivivor0519@gmail.com>
2020-05-20 16:48:56 +0900

    //スルメです

---
+++ ポップスズ5月製作/surume.cpp
@@ -0,0 +1,88 @@
+#include "main.h"
+#include "surume.h"
+#include "player.h"
+#include "hitCheck.h"
+
+CHARACTER surume[SURUME_MAX];
+int surumeImage;
+//ロード
+void SysInitSurume()
+{
+	surumeImage = LoadGraph("bmp/イカスルメ.png");
+}
+//初期化
+void InitSurume()
+{
+	for (int i = 0; i < SURUME_MAX; i++)
+	{
+		surume[i].pos = { 0, 0 };
+		surume[i].speed = { 1,1 };
+		surume[i].count = 0;
+		surume[i].flag = false;
+		surume[i].size = { 32, 32 };
+		surume[i].offSet = { surume[i].size.x / 2, surume[i].size.y / 2 };
+		surume[i].hp = 30;		// イカ焼き取得時に回復するライフの量
+		surume[i].onlyOnce = false;
+	}
+}
+//更新
+void UpdetaSurume(XY pos, bool flag, int num)
+{
+	if (!surume[num].onlyOnce)
+	{
+		if (!surume[num].flag)
+		{
+			surume[num].pos = pos;
+			surume[num].flag = flag;
+			surume[num].onlyOnce = true;
+		}
+	}
+
+
+	if (surume[num].flag)
+	{
+		surume[num].pos.y += surume[num].speed.y;
+
+		if (surume[num].pos.y > SCREEN_SIZE_Y)
+		{
+			surume[num].flag = false;
+		}
+
+		if (HitCheckRectToRect(player, num, surume))		// 矩形と矩形の当たり判定
+		{
+			surume[num].flag = false;
+			if (player.hp + surume[num].hp < TIME_FRAME * PLAYER_HP_MAX)
+			{
+				player.hp = player.hp + surume[num].hp;
+			}
+			if (player.hp + surume[num].hp >= TIME_FRAME * PLAYER_HP_MAX)
+			{
+				player.hp = TIME_FRAME * PLAYER_HP_MAX;
+			}
+		}
+	}
+}
+//描画
+void DrawSurume(int num)
+{
+	if (surume[num].flag)
+	{
+		DrawGraph(surume[num].pos.x - surume[num].offSet.x,
+			surume[num].pos.y - surume[num].offSet.y,
+			surumeImage, true);
+		// 当たり判定の可視化
+		DrawBox(surume[num].pos.x - surume[num].offSet.x,
+			surume[num].pos.y - surume[num].offSet.y,
+			surume[num].pos.x + surume[num].offSet.x,
+			surume[num].pos.y + surume[num].offSet.y,
+			0x000000, false);
+	}
+}
+
+void DeleteSurume()
+{
+	for (int i = 0; i < SURUME_MAX; i++)
+	{
+		surume[i].flag = false;
+	}
+}
---
+++ ポップスズ5月製作/surume.h
@@ -0,0 +1,10 @@
+#pragma once
+#define SURUME_MAX 9
+
+extern CHARACTER surume[SURUME_MAX];
+
+void SysInitSurume();
+void InitSurume();
+void UpdetaSurume(XY pos, bool flag, int num);
+void DrawSurume(int num);
+void DeleteSurume();
